390ebc7cbc6d237bb7c5d098c76ca5dbccaa6c4b
Adrian Javorski <adrianjavorski@gmail.com>
2020-10-08 12:12:22 -0700

    chore: Merged with main to have updated code

--- client/testscene.js
+++ client/testscene.js
@@ -1,13 +1,13 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 export default class TestScene extends Phaser.Scene {
   constructor() {
-    super({ key: "TestScene" });
+    super({ key: 'TestScene' });
   }
 
   preload() {
-    this.load.image("grass", "POCMap.png");
-    this.load.tilemapTiledJSON("mappy", "POCmappy.json");
+    this.load.image('grass', 'POCMap.png');
+    this.load.tilemapTiledJSON('mappy', 'POCmappy.json');
   }
 
   create() {
@@ -17,15 +17,24 @@ export default class TestScene extends Phaser.Scene {
     this.spaceBar = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     );
-
+    this.leftMouse = this.input.on('pointerdown', () => {
+      this.projectile = this.add.rectangle(1024 / 2, 768 / 2, 10, 10, 0xff0000);
+      this.projectile.rotation = this.rect.rotation;
+      this.physics.add.existing(this.projectile);
+      this.projectile.body.setVelocity(50, -50);
+      console.log(this.projectile.rotation);
+    });
+    this.shoot = () => {
+      this.projectile.rotation = rotation;
+    };
     this.gen = () => {
       let genRect = this.add.rectangle(1024 / 2, 768 / 2, 128, 128, 0xff0000);
       this.physics.add.existing(genRect);
       genRect.body.setVelocity(100);
     };
-    let mappy = this.add.tilemap("mappy");
-    let terrarian = mappy.addTilesetImage("Tilly", "grass");
-    let grassLayer = mappy.createStaticLayer("Tile Layer 1", [terrarian], 0, 0);
+    let mappy = this.add.tilemap('mappy');
+    let terrarian = mappy.addTilesetImage('Tilly', 'grass');
+    let grassLayer = mappy.createStaticLayer('Tile Layer 1', [terrarian], 0, 0);
   }
 
   update() {
@@ -35,7 +44,9 @@ export default class TestScene extends Phaser.Scene {
       1024 / 2,
       768 / 2
     );
-
+    if (this.leftMouse.isDown) {
+      console.log('click');
+    }
     if (this.spaceBar.isDown) {
       this.gen();
     }
