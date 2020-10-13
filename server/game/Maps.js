bad1d4fca278942ef15b4574a7f3f82ce1f20d6d
David Moon <moon.kyoungje@gmail.com>
2020-10-13 13:25:48 -0400

    fix: Map Class

--- server/game/Maps.js
+++ server/game/Maps.js
@@ -12,17 +12,17 @@ module.exports = class Map {
   }
 
   createMap() {
-    let mappy = this.scene.add.tilemap("mappy");
-    let terrarian = mappy.addTilesetImage("Base", "");
-    let grassLayer = mappy.createStaticLayer("Grass", [terrarian], 0, 0);
+    const mappy = this.scene.add.tilemap("mappy");
+    const terrarian = mappy.addTilesetImage("Base", "");
+    const grassLayer = mappy.createStaticLayer("Grass", [terrarian], 0, 0);
     grassLayer.setDepth(-1);
-    let top = mappy.createStaticLayer("Collides", [terrarian], 0, 0);
-    let houseTop = mappy.createStaticLayer("HouseTop", [terrarian], 0, 0);
-    let trees = mappy.createStaticLayer("Trees", [terrarian], 0, 0);
+    const top = mappy.createStaticLayer("Collides", [terrarian], 0, 0);
+    const houseTop = mappy.createStaticLayer("HouseTop", [terrarian], 0, 0);
+    const trees = mappy.createStaticLayer("Trees", [terrarian], 0, 0);
     this.collidesPlayer.add(top);
-    this.collidesPlayer.add(tree);
+    this.collidesPlayer.add(trees);
     this.collidesBullets.add(top);
-    this.collidesBullets.add(tree);
+    this.collidesBullets.add(trees);
 
     top.setCollisionByProperty({ collides: true });
     trees.setCollisionByProperty({ collides: true });
